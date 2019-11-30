cc17b0f4af7e240247c34eef993637c6b79e27fe
DaisukeSaito-SAI <rlst.story@gmail.com>
2019-11-30 07:49:42 +0000

    toukou

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
   def new
   end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  def new
+    @post = Post.new()
+  end
+  
+  def create
+    @post = Post.new(user_params)
+    if @post.save
+      redirect_to "/"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:post).permit(:content)
+    end
+  
 end
