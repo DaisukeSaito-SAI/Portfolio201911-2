e6d91188cff829f9484b65f5268e0c74f8340cc5
DaisukeSaito-SAI <rlst.story@gmail.com>
2019-12-06 05:58:50 +0000

    Remember me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191205142747) do
+ActiveRecord::Schema.define(version: 20191206052013) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20191205142747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
